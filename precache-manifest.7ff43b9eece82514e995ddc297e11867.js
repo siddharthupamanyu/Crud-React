2daf1f49d54827842812b3c59ecabe46649fedaa
munish13490 <munisiiii@gmail.com>
2019-12-05 08:42:39 +0530

    Updates

---
+++ precache-manifest.7ff43b9eece82514e995ddc297e11867.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "031563636395bbb862d2fcd5bd94318d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d488fc80fd2d2d837430",
+    "url": "/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "a52afdc654ccb19abc65",
+    "url": "/static/js/2.cf713ebc.chunk.js"
+  },
+  {
+    "revision": "d488fc80fd2d2d837430",
+    "url": "/static/js/main.a519d7fe.chunk.js"
+  },
+  {
+    "revision": "af0b0c216b143e570187",
+    "url": "/static/js/runtime-main.a690ed45.js"
+  }
+]);
